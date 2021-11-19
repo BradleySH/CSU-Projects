6f220aba544f48ad5c880f6c02d3ec2a4b8612e2
Bradley Horlander <78397249+BradleySH@users.noreply.github.com>
2021-11-18 22:43:55 -0500

    CSU courses in Java

---
+++ TryCatch/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] colors = new String[3];
+    }
+}
+
+
+
+    //Write program with a method that returns an array.
+// The method should accept as input a comma-delimited string with three values from a user.
+// The array should store each value in a different element.
+// Use Try..Catch error handling and print any failure messages,
+// or print success from within method if execution is successful (see Chapter 6 in the text).
+// Call the method from the main method of the program to demonstrate its functionality by looping through the array and printing the individual values.
